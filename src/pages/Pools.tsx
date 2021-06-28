4aed4ad49965ca98ec905e64e71ec3f5a062a0d1
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-06-28 19:45:17 -0400

    Update Pools.tsx

--- src/pages/Pools.tsx
+++ src/pages/Pools.tsx
@@ -13,7 +13,7 @@ import usePoolData from "../hooks/usePoolData"
 function Pools(): ReactElement | null {
   const [btcPoolData, btcUserShareData] = usePoolData(BTC_POOL_NAME)
   const [usdPoolData, usdUserShareData] = usePoolData(STABLECOIN_POOL_NAME)
-  const [veth2PoolData, veth2UserShareData] = usePoolData(VETH2_POOL_NAME)
+  //const [veth2PoolData, veth2UserShareData] = usePoolData(VETH2_POOL_NAME)
   const [alethPoolData, alethUserShareData] = usePoolData(ALETH_POOL_NAME)
 
   return (
