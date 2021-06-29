7a0929ecb623a417d4285233607943d301acf497
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-06-29 07:43:44 -0400

    Update DepositPage.tsx

--- src/components/DepositPage.tsx
+++ src/components/DepositPage.tsx
@@ -214,7 +214,8 @@ const DepositPage = (props: Props): ReactElement => {
           {currentModal === "confirm" ? <ConfirmTransaction /> : null}
         </Modal>
       </div>
-    )</div>
+    </div>
+    )
   )
 }
 export default DepositPage
