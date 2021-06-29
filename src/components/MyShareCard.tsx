1919ec9f79671f757412be486398b87a83bfa774
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-06-29 07:04:30 -0400

    Update MyShareCard.tsx

--- src/components/MyShareCard.tsx
+++ src/components/MyShareCard.tsx
@@ -1,6 +1,6 @@
 import "./MyShareCard.scss"
 
-import { Partners, UserShareType } from "../hooks/usePoolData"
+import { UserShareType } from "../hooks/usePoolData"
 import React, { ReactElement } from "react"
 import { formatBNToPercentString, formatBNToString } from "../utils"
 
@@ -22,15 +22,6 @@ function MyShareCard({ data }: Props): ReactElement | null {
     share: formatBNToPercentString(data.share, 18),
     usdBalance: commify(formatBNToString(data.usdBalance, 18, 2)),
     amount: commify(formatBNToString(data.underlyingTokensAmount, 18, 6)),
-    amountsStaked: Object.keys(data.amountsStaked).reduce((acc, key) => {
-      const value = data.amountsStaked[key as keyof typeof data.amountsStaked]
-      return value
-        ? {
-            ...acc,
-            [key]: commify(formatBNToString(value, 18, 6)),
-          }
-        : acc
-    }, {} as typeof data.amountsStaked),
     tokens: data.tokens.map((coin) => {
       const token = TOKENS_MAP[coin.symbol]
       return {
@@ -39,11 +30,6 @@ function MyShareCard({ data }: Props): ReactElement | null {
         value: commify(formatBNToString(coin.value, 18, 6)),
       }
     }),
-  }
-  const stakingUrls = {
-    keep: "https://dashboard.keep.network/liquidity",
-    sharedStake: "https://dashboard.keep.network/liquidity",
-    alchemix: "https://app.alchemix.fi/farms",
   }
 
   return (
@@ -62,18 +48,6 @@ function MyShareCard({ data }: Props): ReactElement | null {
         <div className="infoItem">
           <span className="label bold">{`${t("totalAmount")}: `}</span>
           <span className="value">{formattedData.amount}</span>
-          {Object.keys(data.amountsStaked).map((key) => {
-            return data.amountsStaked[key as Partners]?.gt(Zero) ? (
-              <span className="value">
-                &nbsp;
-                <a
-                  href={stakingUrls[key as Partners]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  ({formattedData.amountsStaked[key as Partners]} {t("staked")})
-                </a>
-              </span>
             ) : null
           })}
         </div>
