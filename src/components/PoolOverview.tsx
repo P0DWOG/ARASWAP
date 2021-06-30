e25417c11c905e05a3221b386195020edca46c19
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-06-29 21:47:55 -0400

    Update PoolOverview.tsx

--- src/components/PoolOverview.tsx
+++ src/components/PoolOverview.tsx
@@ -77,8 +77,7 @@ function PoolOverview({
         <div className="poolInfo">
           {Object.keys(poolData.aprs).map((key) => {
             const symbol = poolData.aprs[key as Partners]?.symbol as string
-            return poolData.aprs[key as Partners]?.apr.gt(Zero) ? (
-              <div className="margin Apr" key={symbol}>
+            return poolData.aprs[key as Partners]?.apr.gt(Zero) ? (<div className="margin Apr" key={symbol}>
               </div>
             ) : null
           })}
