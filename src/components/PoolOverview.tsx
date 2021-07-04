766ee8259ea2c6f8d1d63f7967396af4f9d28804
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-07-03 20:14:44 -0400

    Update PoolOverview.tsx

--- src/components/PoolOverview.tsx
+++ src/components/PoolOverview.tsx
@@ -64,14 +64,8 @@ function PoolOverview({
         )}
         <div className="tokens">
           <span style={{ marginRight: "8px" }}>[</span>
-          {formattedData.tokens.map(({ symbol, icon }) => (
-            <div className="token" key={symbol}>
-              <img alt="icon" src={icon} />
-              <span>{symbol}</span>
-            </div>
           <span style={{ marginLeft: "-8px" }}>]</span>
         </div>
-      ))}
       <div className="right">
         <div className="poolInfo">
           {Object.keys(poolData.aprs).map((key) => {
