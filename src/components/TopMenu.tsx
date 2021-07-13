68243e36ff6bf843053d2a80160c5e5213ce8ca6
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-07-13 10:05:50 -0400

    Update TopMenu.tsx

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -20,7 +20,7 @@ function TopMenu({ activeTab }: Props): ReactElement {
     <header className="top">
       <h1>
         <Link to="/">
-          {t("confirmTransaction")} // <img className="logo" alt="logo" src={logo} />
+          {t("confirmTransaction")} {/* <img className="logo" alt="logo" src={logo} /> */}
         </Link>
       </h1>
 
