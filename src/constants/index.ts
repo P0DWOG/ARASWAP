31ce0732059a676a813fdc8c049f02d1b5c07222
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-08-10 21:25:42 -0400

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,7 +20,7 @@ import wethLogo from "../assets/icons/weth.svg"
 
 export const NetworkContextName = "NETWORK"
 export const BTC_POOL_NAME = "BTC Portal "
-export const STABLECOIN_POOL_NAME = "USD Portal "
+export const STABLECOIN_POOL_NAME = "Earth Dollars (US)"
 export const VETH2_POOL_NAME = "vETH2 Portal "
 export const ALETH_POOL_NAME = "ETH Portal "
 export type PoolName =
