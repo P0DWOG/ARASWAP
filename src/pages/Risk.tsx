807d78d71f09c46ec8c474cce5286806712c2c33
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-08-15 12:59:38 -0400

    Update Risk.tsx

--- src/pages/Risk.tsx
+++ src/pages/Risk.tsx
@@ -14,7 +14,7 @@ function Risk(): ReactElement {
       <div className="content">
         <p>
           {t("riskIntro")}{" "}
-          <a href="https://github.com/MuneFinance/MuneFinance">
+          <a href="https://github.com/MuneFinance/">
             {t("riskIntro2")}
           </a>{" "}
           {t("riskIntro3")}
